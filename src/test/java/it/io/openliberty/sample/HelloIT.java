212581be5df7dd2acb9164de17ed2b3d8f0e5a1b
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2020-10-22 15:06:22 -0400

    Use local build of liberty arquillian
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

--- src/test/java/it/io/openliberty/sample/HelloIT.java
+++ src/test/java/it/io/openliberty/sample/HelloIT.java
@@ -2,24 +2,21 @@ package it.io.openliberty.sample;
 
 import java.net.URL;
 
-import org.apache.cxf.jaxrs.client.WebClient;
-import org.apache.cxf.jaxrs.provider.jsrjsonp.JsrJsonpProvider;
+// import org.apache.cxf.jaxrs.provider.jsrjsonp.JsrJsonpProvider;
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
+// import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Assert;
+// import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import io.openliberty.sample.system.SimpleHello;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.Response;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(Arquillian.class)
 public class HelloIT {
@@ -37,19 +34,12 @@ public class HelloIT {
     private URL baseURL;
 
     @Inject
-    SimpleHello simpleHello;
+    private SimpleHello simpleHello;
 
     @Test
-    @RunAsClient
+    // @RunAsClient
     public void testHelloEndpoint() {
-        Client client = ClientBuilder.newClient();
-        client.register(JsrJsonpProvider.class);
-
-        WebTarget target = client.target(baseURL + "/hello");
-        Response response = target.request().get();
-
-        System.out.println("STATUS: " + response.getStatus());
-        Assert.assertEquals("Incorrect response code from " + baseURL, 200, response.getStatus());
+        assertNotNull("SimpleHello should be injectable", simpleHello);
     }
 
 }
